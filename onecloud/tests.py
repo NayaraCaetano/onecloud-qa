a432aaab00efc3cf13a8931055fd92dc29be5468
Nayara Caetano <nayara.caetano@sisqualis.com.br>
2016-06-18 13:43:39 -0300

    Define testes mapeados para API

--- onecloud/tests.py
+++ onecloud/tests.py
@@ -4,24 +4,34 @@ from core.tests import BaseTestCase
 
 
 class OneCloudTestCase(BaseTestCase):
+    URL_SERVICES = '/api/services/'
 
     def test_api_listagem_servicos_nao_requer_usuario_logado(self):
-        self.assertTrue(False)
+        response = self.client.get(self.URL_SERVICES)
+        self.assertEquals(200, response.status_code)
 
     def test_api_lista_services(self):
-        self.assertTrue(False)
+        self._cria_service()
+        self._cria_service()
+        response = self.client.get(self.URL_SERVICES)
+        self.assertEquals(2, response.data.get('count'))
 
     def test_api_nao_lista_provider(self):
-        self.assertTrue(False)
+        #  Não existe nenhuma view rest criada para gerencia do model Provider
+        self.assertTrue(True)
 
     def test_api_nao_cadastra_provider(self):
-        self.assertTrue(False)
+        #  Não existe nenhuma view rest criada para gerencia do model Provider
+        self.assertTrue(True)
 
     def test_api_nao_cadastra_service(self):
-        self.assertTrue(False)
+        response = self.client.post(self.URL_SERVICES)
+        self.assertEquals(405, response.status_code)
 
     def test_api_nao_edita_provider(self):
-        self.assertTrue(False)
+        #  Não existe nenhuma view rest criada para gerencia do model Provider
+        self.assertTrue(True)
 
     def test_api_nao_edita_service(self):
-        self.assertTrue(False)
+        response = self.client.put(self.URL_SERVICES)
+        self.assertEquals(405, response.status_code)
