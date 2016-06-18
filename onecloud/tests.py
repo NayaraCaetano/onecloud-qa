cfa901679607c84d391b2dcc776537df427b007e
Nayara Caetano <nayara.caetano@sisqualis.com.br>
2016-06-18 12:48:24 -0300

    Adiciona mapa de regras testadas para a API

---
+++ onecloud/tests.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+from core.tests import BaseTestCase
+
+
+class OneCloudTestCase(BaseTestCase):
+
+    def test_api_listagem_servicos_nao_requer_usuario_logado(self):
+        self.assertTrue(False)
+
+    def test_api_lista_services(self):
+        self.assertTrue(False)
+
+    def test_api_nao_lista_provider(self):
+        self.assertTrue(False)
+
+    def test_api_nao_cadastra_provider(self):
+        self.assertTrue(False)
+
+    def test_api_nao_cadastra_service(self):
+        self.assertTrue(False)
+
+    def test_api_nao_edita_provider(self):
+        self.assertTrue(False)
+
+    def test_api_nao_edita_service(self):
+        self.assertTrue(False)
