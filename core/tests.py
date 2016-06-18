18138451aab702d1a6c79200d52e482f698e4089
Nayara Caetano <nayara.caetano@sisqualis.com.br>
2016-06-18 12:41:08 -0300

    Adiciona estrutura inicial para testes caixa preta
    - Clase base para tescase

---
+++ core/tests.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+import factory
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from faker import Faker
+from rest_framework.test import APIClient
+
+from onecloud.models import Provider, Service
+
+faker = Faker('pt_BR')
+
+
+class BaseTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def _cria_usuario(self, **kwargs):
+        return UsuarioFactory.create(**kwargs)
+
+    def _cria_provider(self, **kwargs):
+        return ProviderFactory.create(**kwargs)
+
+    def _cria_service(self, **kwargs):
+        return ServiceFactory.create(**kwargs)
+
+
+class UsuarioFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = faker.sha1()
+
+
+class ProviderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Provider
+
+    name = faker.text()
+
+
+class ServiceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Service
+
+    name = faker.text()
+    provider = factory.SubFactory(ProviderFactory)
+    cpu = faker.random_number()
+    memory = faker.random_number()
+    disk = faker.random_number()
+    price = faker.random_number()
--- settings/base.py
+++ settings/base.py
@@ -29,7 +29,8 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    'onecloud'
+    'onecloud',
+    'core'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
